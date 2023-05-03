c0417dd08022af06efd7fe7a07df30fb2dd937a8
schlin <linuslauer@proton.me>
2023-05-03 13:53:45 +0200

    Added card-list

--- src/app/cards/card-list/card-list.component.ts
+++ src/app/cards/card-list/card-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { invoke } from "@tauri-apps/api";
-import { throwError } from "rxjs";
+import { WebviewWindow } from "@tauri-apps/api/window";
+import { invoke, tauri } from "@tauri-apps/api";
+import { Observable, from, throwError } from "rxjs";
 import { Card, CardDB } from "src/app/model/card";
 import { CardService } from "src/app/services/card.service";
 
@@ -8,27 +9,20 @@ import { CardService } from "src/app/services/card.service";
   selector: "app-card-list",
   template: `
     <div class="list-container">
-      <mat-card>
-        <mat-card-header>
-          <mat-card-title>Actions Buttons</mat-card-title>
-          <mat-card-subtitle>Start</mat-card-subtitle>
-        </mat-card-header>
-        <mat-card-actions>
-          <button mat-button>LIKE</button>
-          <button mat-button>SHARE</button>
-        </mat-card-actions>
-      </mat-card>
-      <br />
-      <mat-card>
-        <mat-card-header>
-          <mat-card-title>Actions Buttons</mat-card-title>
-          <mat-card-subtitle>Start</mat-card-subtitle>
-        </mat-card-header>
-        <mat-card-actions>
-          <button mat-button>LIKE</button>
-          <button mat-button>SHARE</button>
-        </mat-card-actions>
-      </mat-card>
+      <ng-container *ngFor="let card of allCards | async">
+        <mat-card>
+          <mat-card-header>
+            <mat-card-title>{{ card.title }}</mat-card-title>
+            <mat-card-subtitle>{{ card.description }}</mat-card-subtitle>
+          </mat-card-header>
+          <mat-card-actions>
+            <button mat-button (click)="goToDetailsPage(card.id, card.title)">
+              Bearbeiten
+            </button>
+          </mat-card-actions>
+        </mat-card>
+        <br />
+      </ng-container>
     </div>
   `,
   styles: [
@@ -44,13 +38,20 @@ import { CardService } from "src/app/services/card.service";
   ],
 })
 export class CardListComponent implements OnInit {
-  public allCards: CardDB[] = [];
+  public allCards: Observable<CardDB[]>;
 
   constructor(private cardService: CardService) {}
 
   ngOnInit(): void {
-    this.cardService.readCards().then((res) => {
-      this.allCards = res;
+    this.allCards = from(this.cardService.readCards());
+  }
+
+  goToDetailsPage(cardId: number, cardTitle: string) {
+    const webview = new WebviewWindow(cardId.toString(), {
+      url: "cards/details?id=" + cardId,
+    });
+    webview.once("tauri://error", function (e) {
+      console.log("window creation error: " + JSON.stringify(e));
     });
   }
 }
