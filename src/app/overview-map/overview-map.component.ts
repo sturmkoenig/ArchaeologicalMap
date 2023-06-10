7d3fc16c94e67ffcb1e37c6ce5f12a771f0030ae
schlin <linuslauer@proton.me>
2023-06-10 10:17:12 +0200

    fixed icon flicker and popup closse bug

--- src/app/overview-map/overview-map.component.ts
+++ src/app/overview-map/overview-map.component.ts
@@ -54,9 +54,10 @@ export interface Compas {
 })
 export class OverviewMapComponent implements OnInit, AfterViewInit {
   layers: Layer[] = [];
-  displayedMarkers = [];
   position?: LatLng;
+  lastFetchedMarkerIds?: number[];
   layerGroup: LayerGroup = new LayerGroup();
+  markerIdLayerMap: [number, Layer][] = [];
   options: MapOptions = {
     layers: [
       tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -85,6 +86,7 @@ export class OverviewMapComponent implements OnInit, AfterViewInit {
   onMapReady(map: Map) {
     this.map = map;
     this.zoom = map.getZoom();
+    this.map.addLayer(this.layerGroup);
   }
 
   mapChanged(emittedMap: Map) {
@@ -107,34 +109,69 @@ export class OverviewMapComponent implements OnInit, AfterViewInit {
   mapMoveEnded($event: any) {
     let bounds = this.map.getBounds();
 
-    if (this.map.getZoom() < 8) {
+    if (this.map.getZoom() < 7) {
       this.map.removeLayer(this.layerGroup);
+      this.layerGroup = new LayerGroup();
+      this.map.addLayer(this.layerGroup);
+      this.lastFetchedMarkerIds = [];
+      this.markerIdLayerMap = [];
       return;
     }
     this.markerService
-      .updateMarkersInArea({
+      .getMarkersInArea({
         north: bounds.getNorth(),
         east: bounds.getEast(),
         south: bounds.getSouth(),
         west: bounds.getWest(),
       })
       .then((cardMarkers) => {
-        this.map.removeLayer(this.layerGroup);
-        let tempLayerGroup = new LayerGroup();
+        let newFetchedMarkerIds = cardMarkers.map((marker) => marker.markerId);
+        let markersToAdd: number[];
+        let markersToRemove: number[];
+        if (this.lastFetchedMarkerIds) {
+          [markersToRemove, markersToAdd] = this.markerDiff(
+            this.lastFetchedMarkerIds,
+            newFetchedMarkerIds
+          );
+        } else {
+          markersToAdd = newFetchedMarkerIds;
+          markersToRemove = [];
+        }
 
-        cardMarkers.forEach((marker) => {
-          console.log(marker[0]);
-          console.log(this.layerGroup.getLayers());
-          console.log(this.layerGroup.hasLayer(marker[0]));
-          this.layerGroup.addLayer(marker[0]);
-          this.createHoverCoordinateRadius(marker[0], marker[1]);
-          // this.layerGroup.addLayer(marker[0]);
-          // this.createHoverCoordinateRadius(marker[0], marker[1]);
-        });
-        console.log("markers: " + cardMarkers);
-        this.map.addLayer(this.layerGroup);
+        this.markerIdLayerMap
+          .filter((markerMap) => markersToRemove.indexOf(markerMap[0]) > -1)
+          .forEach((markerMap) => {
+            this.layerGroup.removeLayer(markerMap[1]);
+          });
+
+        this.markerIdLayerMap = this.markerIdLayerMap.filter(
+          (markerMap) => markersToRemove.indexOf(markerMap[0]) < 0
+        );
+
+        cardMarkers
+          .filter((marker) => markersToAdd.indexOf(marker.markerId) > -1)
+          .forEach((marker) => {
+            this.layerGroup.addLayer(marker.marker);
+            this.markerIdLayerMap.push([marker.markerId, marker.marker]);
+            this.createHoverCoordinateRadius(marker.marker, marker.radius);
+          });
+        this.lastFetchedMarkerIds = newFetchedMarkerIds;
       });
   }
+
+  markerDiff(
+    lastFetchedMarkerIds: number[],
+    newFetchedMarkerIds: number[]
+  ): [number[], number[]] {
+    let markersToRemove = lastFetchedMarkerIds.filter(
+      (lastMarker) => newFetchedMarkerIds.indexOf(lastMarker) < 0
+    );
+    let markersToAdd = newFetchedMarkerIds.filter(
+      (newMarker) => lastFetchedMarkerIds.indexOf(newMarker) < 0
+    );
+    return [markersToRemove, markersToAdd];
+  }
+
   ngAfterViewInit(): void {
     if (
       this.route.snapshot.queryParams["longitude"] &&
@@ -151,6 +188,7 @@ export class OverviewMapComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     appWindow.setTitle("map");
   }
+
   panToLatLng(lat: number, lng: number) {
     this.map.flyTo({ lat: lat, lng: lng });
   }
--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -18,6 +18,11 @@ import { IconService } from "./icon.service";
 import { Router } from "@angular/router";
 import { v4 as uuidv4 } from "uuid";
 
+interface CardMarkerLayer {
+  markerId: number;
+  marker: Marker;
+  radius: Layer | null;
+}
 interface CardTitleMapping {
   id: number;
   title: string;
@@ -38,45 +43,46 @@ export class MarkerService {
     });
   }
 
-  updateMarkersInArea(
-    directions: CardinalDirection
-  ): Promise<[Marker, Layer | null][]> {
+  getMarkersInArea(directions: CardinalDirection): Promise<CardMarkerLayer[]> {
     return this.cardService
       .readMarkersInArea(directions)
       .then((markersDB: MarkerDB[]) => {
-        let markers: [Marker, Layer | null][] = [];
+        let markers: CardMarkerLayer[] = [];
         markersDB.forEach((markerDB) => {
-          let icon = new Icon({
-            iconUrl: this.iconService
-              .getIconPath(markerDB.icon_name)
-              .toString(),
-            iconSize: [20, 20],
-            popupAnchor: [0, 0],
-          });
-          let iconMarker = new Marker([markerDB.latitude, markerDB.longitude], {
-            icon,
-            interactive: true,
-          });
-          // TODO get card title from cache
-          let title: string =
-            this.cardTitleMapping.find((obj) => obj.id === markerDB.card_id)
-              ?.title ?? "Kein Titel Vorhanden";
-          const div: HTMLDivElement = createPopupHTML(markerDB, title);
-          iconMarker.bindPopup(div);
-          if (markerDB.radius !== 0.0) {
-            let circle = new Circle([markerDB.latitude, markerDB.longitude], {
-              className: "fade-in",
-              radius: markerDB.radius,
-            });
-            markers.push([iconMarker, circle]);
-          } else {
-            markers.push([iconMarker, null]);
-          }
+          markers.push(this.markerToMapLayer(markerDB));
         });
         return markers;
       });
   }
+
+  markerToMapLayer(markerDB: MarkerDB): CardMarkerLayer {
+    let icon = new Icon({
+      iconUrl: this.iconService.getIconPath(markerDB.icon_name).toString(),
+      iconSize: [20, 20],
+      popupAnchor: [0, 0],
+    });
+    let iconMarker = new Marker([markerDB.latitude, markerDB.longitude], {
+      icon,
+      interactive: true,
+    });
+    // TODO get card title from cache
+    let title: string =
+      this.cardTitleMapping.find((obj) => obj.id === markerDB.card_id)?.title ??
+      "Kein Titel Vorhanden";
+    const div: HTMLDivElement = createPopupHTML(markerDB, title);
+    iconMarker.bindPopup(div);
+    if (markerDB.radius !== 0.0) {
+      let circle = new Circle([markerDB.latitude, markerDB.longitude], {
+        className: "fade-in",
+        radius: markerDB.radius,
+      });
+      return { markerId: markerDB.id, marker: iconMarker, radius: circle };
+    } else {
+      return { markerId: markerDB.id, marker: iconMarker, radius: null };
+    }
+  }
 }
+
 function createPopupHTML(marker: MarkerDB, title: string): HTMLDivElement {
   const div: HTMLDivElement = document.createElement("div");
   div.innerHTML = `<h3>` + title + `</h3>`;
