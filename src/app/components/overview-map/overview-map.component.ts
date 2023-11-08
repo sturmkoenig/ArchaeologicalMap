17b6549ffa8c51e822da4ab8b22c0b77745c9ad8
sturmkoenig <linuslauer@protonmail.com>
2023-11-08 16:24:54 +0100

    removed falsy import

--- src/app/components/overview-map/overview-map.component.ts
+++ src/app/components/overview-map/overview-map.component.ts
@@ -15,7 +15,6 @@ import {
 } from "leaflet";
 import { MarkerService } from "../../services/marker.service";
 import { appWindow } from "@tauri-apps/api/window";
-import { W } from "@tauri-apps/api/event-30ea0228";
 
 export interface Compas {
   north: number;
