38c1d45a4c4e0e43bc37535c7c39c28ddcc27274
linus lauer <linus.lauer@codecentric.de>
2024-06-16 11:29:43 +0200

    changed test runner from jasmine to jest

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create the app", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'demo-angular-jest'`, () => {
+    expect(component.title).toEqual("demo-angular-jest");
+  });
+
+  it("should render the title", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "demo-angular-jest app is running!",
+    );
+  });
+});
