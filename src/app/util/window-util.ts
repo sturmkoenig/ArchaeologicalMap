009b734ef5da9aa73480157d5fafcd43c529b1cb
sturmkoenig <linuslauer@protonmail.com>
2025-01-31 12:56:24 +0100

    small improvment in window focusing

--- src/app/util/window-util.ts
+++ src/app/util/window-util.ts
@@ -1,15 +1,21 @@
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { getAllWebviews } from "@tauri-apps/api/webview";
+import { emitTo } from "@tauri-apps/api/event";
 
-export const createCardDetailsWindow = (cardId: number) => {
+export const createCardDetailsWindow = async (cardId: number) => {
+  await getAllWebviews().then((views) =>
+    views.map((view) => console.log(view.label)),
+  );
   const webview = new WebviewWindow(cardId.toString(), {
     url: "/cards/details/" + cardId,
     height: 800,
   });
   webview.once("tauri://error", function (e: unknown) {
     console.error("window creation error: " + JSON.stringify(e));
-    webview.emit(`set-focus-to-${cardId}`);
+    emitTo(cardId.toString(), "set-focus-to");
   });
   webview.once("tauri://created", function () {
+    console.log("created");
     webview.emit(`set-focus-to-${cardId}`);
   });
 };
