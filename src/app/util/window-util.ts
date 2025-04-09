6a2adcd6f4949ec09bbfdaf9577a2557be58475b
linus lauer <linus.lauer_codecentric@dtad.onmicrosoft.com>
2025-04-09 17:15:04 +0200

    removed log from window-util.ts

--- src/app/util/window-util.ts
+++ src/app/util/window-util.ts
@@ -1,11 +1,7 @@
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { getAllWebviews } from "@tauri-apps/api/webview";
 import { emit } from "@tauri-apps/api/event";
 
 export const createCardDetailsWindow = async (cardId: number) => {
-  await getAllWebviews().then((views) =>
-    views.map((view) => console.log(view.label)),
-  );
   const webview = new WebviewWindow(cardId.toString(), {
     url: "/cards/details/" + cardId,
     height: 800,
