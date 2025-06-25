3f80be39aa491e580a0340bbf53cd06ad3710d98
linus lauer <linuslauer@protonmail.com>
2025-06-25 17:27:24 +0200

    draft refactore card details store

--- src/app/components/cards/card-details/card-details.component.ts
+++ src/app/components/cards/card-details/card-details.component.ts
@@ -1,45 +1,34 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, inject, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { emit, listen } from "@tauri-apps/api/event";
 
 import { MatDialog } from "@angular/material/dialog";
 import { getCurrentWindow } from "@tauri-apps/api/window";
-import { Observable } from "rxjs";
 import { EditorComponent } from "@app/layout/editor/editor.component";
-import { Card, InfoCard, isLocationCard, LocationCard } from "@app/model/card";
+import { InfoCard, isLocationCard, LocationCard } from "@app/model/card";
 import { CardContentService } from "@service/card-content.service";
-import { CardDetailsStore } from "@app/state/card-details.store";
-import { ImageEntity } from "@app/model/image";
 import { AddCardDialogComponent } from "../card-input/add-card-dialog.component";
 import { CardService } from "@service/card.service";
+import { CardDetailsSignalStore } from "@app/state/card-details-signal.store";
 
 @Component({
   selector: "app-card-details",
   templateUrl: "./card-details.component.html",
   styleUrls: ["./card-details.component.scss"],
+  providers: [CardDetailsSignalStore],
   standalone: false,
 })
 export class CardDetailsComponent implements OnInit {
-  cardId!: number;
-  card$!: Observable<Card | undefined>;
-
   @ViewChild(EditorComponent)
   editor!: EditorComponent;
-  allCardsInStack$: Observable<Card[]>;
-  currentStackId$: Observable<number | undefined>;
-  regionImage$: Observable<ImageEntity | undefined>;
+  readonly store = inject(CardDetailsSignalStore);
 
   constructor(
     private route: ActivatedRoute,
     public dialog: MatDialog,
     private cardContentService: CardContentService,
-    public cardDetailsStore: CardDetailsStore,
     private cardService: CardService,
   ) {
-    this.allCardsInStack$ = this.cardDetailsStore.allCardsInStack$;
-    this.currentStackId$ = this.cardDetailsStore.currentStackId$;
-    this.card$ = this.cardDetailsStore.currentCard$;
-    this.regionImage$ = this.cardDetailsStore.currentImage$;
     listen("tauri://focus", async () => {
       this.cardContentService.cardContent.next(this.editor.getContents());
       await this.cardContentService.saveCardContent();
@@ -54,24 +43,20 @@ export class CardDetailsComponent implements OnInit {
       }
       const cardId = Number(params.get("cardId"));
       const stackId = Number(params.get("stackId"));
-      this.cardDetailsStore.currentCard$.subscribe((card) => {
-        this.cardId = card?.id ?? 0;
-      });
       if (cardId) {
-        this.cardDetailsStore.loadStackOfCards(cardId);
+        this.store.setCard(cardId);
         listen(`set-focus-to-${cardId}`, async () => {
           await appWindow.setFocus();
         });
       } else if (stackId) {
-        this.cardDetailsStore.loadStack(stackId);
+        this.store.setStack(stackId);
       } else {
         console.error("cardId not provided!");
       }
     });
     listen("card-changed", (_) => {
       // TODO only reload if changed card is in stack!
-      this.cardDetailsStore.resetState$();
-      this.cardDetailsStore.loadStackOfCards(this.cardId);
+      this.store.setCard(this.store.currentCard()?.id ?? 0);
     });
 
     await appWindow.onCloseRequested(async () => {
@@ -89,24 +74,23 @@ export class CardDetailsComponent implements OnInit {
   }
 
   openAddCardDialog() {
-    this.currentStackId$
-      .subscribe((stackId) => {
-        if (!stackId) return;
-        const dialogRef = this.dialog.open(AddCardDialogComponent, {
-          data: { stackId },
-        });
-        dialogRef.afterClosed().subscribe(async (result: InfoCard) => {
-          if (result) {
-            try {
-              await this.cardService.createCard(result);
-              this.cardDetailsStore.loadStack(stackId);
-            } catch (e) {
-              console.error("Failed to create card:", e);
-            }
-          }
-        });
-      })
-      .unsubscribe();
+    const stackId = this.store.stack()?.id;
+    if (!stackId) return;
+
+    const dialogRef = this.dialog.open(AddCardDialogComponent, {
+      data: { stackId },
+    });
+
+    dialogRef.afterClosed().subscribe(async (result: InfoCard) => {
+      if (result) {
+        try {
+          await this.cardService.createCard(result);
+          this.store.setStack(stackId!);
+        } catch (e) {
+          console.error("Failed to create card:", e);
+        }
+      }
+    });
   }
 
   protected readonly isLocationCard = isLocationCard;
